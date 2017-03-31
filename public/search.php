68fc6ef07c215a847bb9ee44f79ca0035e2a7f33
athena3764 <jhhsu11@gmail.com>
2017-03-31 01:06:33 -0700

    Update search.php

--- public/search.php
+++ public/search.php
@@ -10,16 +10,12 @@
     $rows = CS50::query("SELECT * FROM places WHERE MATCH(postal_code, place_name, admin_name1) AGAINST (?)", $_GET["geo"]);
     
     foreach($rows as $row){
-      
      $places[]=[
             "postal_code" => $row["postal_code"],
             "place_name" => $row["place_name"],
             "admin_name1" => $row["admin_name1"]
-            
            ];
-     
    }
-   
    
     // output places as JSON (pretty-printed for debugging convenience)
     header("Content-type: application/json");
